c35d9546bf39fbda464a52af82b9fda4f6a00b1c
jhenderson0 <hendersonjfd@gmail.com>
2026-06-12 09:54:29 +0100

    revise figures

--- code/random_growth_src/lib/empirical_densities.py
+++ code/random_growth_src/lib/empirical_densities.py
@@ -1,5 +1,19 @@
 import numpy as np
+import pandas as pd
 
+# Theoretical well-mixed sampled abundances (x = 0 for all clones)
+def sample_well_mixed(df, C1='count_B', C2='count_T'):
+    
+    counts_1 = df[C1].to_numpy(dtype=np.int64)
+    counts_2 = df[C2].to_numpy(dtype=np.int64)  
+    counts_total = counts_1 + counts_2
+    
+    counts_1_sample  = np.random.binomial(counts_total, 0.5)
+    counts_2_sample = counts_total -  counts_1_sample
+    
+    return pd.DataFrame({C1 :counts_1_sample, C2:counts_2_sample, "count_total": counts_total})
+    
+# Observed log-ratios
 def get_empirical_log_ratios(df, C1='count_B', C2='count_T', threshold=10, base=10):
     
     counts_1 = df[C1].to_numpy(dtype=np.int64)
